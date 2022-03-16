59b656d8b3c3f0a05d74e030824dc8857a14fc73
Tim <timothymeyers@users.noreply.github.com>
2022-03-16 12:15:11 -0400

    Update outputs.tf

--- example/outputs.tf
+++ example/outputs.tf
@@ -1,7 +1,7 @@
-output "aks_cluster" {
-  value = module.rke2_cluster.cluster_data
+output "aks_cluster_name" {
+  value = var.cluster_name 
 }
 
 output "rg_name" {
-  value = local.resource_group_name
+  value = "${var.cluster_name}-rg"
 }
